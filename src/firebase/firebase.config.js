71679157660c5bf81ecd7d670df7245fb5c9ffaa
ifty17 <iftyrahatkabir@gmail.com>
2022-11-21 17:55:57 +0600

    added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  // COnfig code goes here
+  apiKey: "AIzaSyDCUmgxv8WmPXiKlbwleJ-wzCdG0sjK2pw",
+  authDomain: "air-cnc-19b08.firebaseapp.com",
+  projectId: "air-cnc-19b08",
+  storageBucket: "air-cnc-19b08.appspot.com",
+  messagingSenderId: "621750922700",
+  appId: "1:621750922700:web:ce44829920892e98624169",
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
